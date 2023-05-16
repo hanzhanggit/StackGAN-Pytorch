673718281697a8898bf6f66a1a971042b5e031b8
soumensardar <soumen.sardar@smithsdetection.com>
2023-05-16 20:49:57 +0530

    - script to check CUDA support version

--- cuda_test.py
+++ cuda_test.py
@@ -1,3 +1,6 @@
 import torch
 
 print(torch.cuda.is_available())
+torch.zeros(1).cuda()
+
+# https://stackoverflow.com/questions/60987997/why-torch-cuda-is-available-returns-false-even-after-installing-pytorch-with
