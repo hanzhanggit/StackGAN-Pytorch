d7fbb1410a6bcb5b5f69fc0b8a0900339d71c77e
soumensardar <soumen.sardar@smithsdetection.com>
2023-05-19 15:34:38 +0530

    configuration for Sixray500 stage-1

---
+++ sqlite_datagen.sh
@@ -0,0 +1,4 @@
+python data/generate_custom_dataset.py --data_dir data/sixray_500 \
+--fasttext_model /data/fasttext/cc.en.300.bin  \
+--sqlite /data/sixray_caption_db/tip_gai.db --clean --copy_images \
+--dataroot /data/Sixray_easy/
