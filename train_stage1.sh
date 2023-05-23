77b4fd27ff51a2433a9b91b9213f207a9917db86
soumensardar <soumen.sardar@smithsdetection.com>
2023-05-23 14:51:07 +0530

    - Updating scripts and configuration

--- train_stage1.sh
+++ train_stage1.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python code/main.py --data_dir data/sixray_500 --cfg code/cfg/sixray_500_s1.yml
+python code/main.py --cfg code/cfg/sixray_500_s1.yml
--- train_stage2.sh
+++ train_stage2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python code/main.py --data_dir data/sixray_500 --cfg code/cfg/sixray_500_s2.yml
+python code/main.py --cfg code/cfg/sixray_500_s2.yml
