213f17868335fb867e299949bbbed2f9cc37419b
soumensardar <soumen.sardar@smithsdetection.com>
2023-06-13 10:37:58 +0530

    - Finetune Experiment15. - Faster LR - GIT file adjustment

--- test_stage1.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-sh code/miscc/cuda_mem.sh
-python code/main.py --test_phase --manualSeed 47 --cfg code/cfg/sixray_500_s1.yml --NET_G output/sixray_2381_ftt_1024D_cbow_nocrop_stage_1_train_2023_06_08_18_48_49\Model\netG_epoch_1000.pth --NET_D output/sixray_2381_ftt_1024D_cbow_nocrop_stage_1_train_2023_06_08_18_48_49\Model\netD_epoch_last.pth
--- train_stage2.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-sh code/miscc/cuda_mem.sh
-python code/main.py --cfg code/cfg/sixray_500_s2.yml --manualSeed 47 --STAGE1_G output/sixray_2381_ftt_1024D_cbow_nocrop_stage_1_train_2023_06_08_18_48_49\Model\netG_epoch_999.pth
